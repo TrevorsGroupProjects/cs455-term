582d6e482e7b215cdce5d4b6099e9a8aae1cd94c
Trevor Holland <tjhollan@colostate.edu>
2022-04-21 14:05:36 -0600

    Update layers and weights

--- src/main/NeuralNetworkPyspark.py
+++ src/main/NeuralNetworkPyspark.py
@@ -1,6 +1,7 @@
 ## 
-## Original source from https://github.com/MarvinMartin24/PySpark-Neural-Network/blob/main/NeuralNetwork.ipynb ##
-##
+## Original source from https://github.com/MarvinMartin24/PySpark-Neural-Network/blob/main/NeuralNetwork.ipynb 
+## and from https://nbviewer.org/url/www.cs.colostate.edu/~cs445/notebooks/07.2%20Optimizers,%20Data%20Partitioning,%20Finding%20Good%20Parameters.ipynb
+## 
 ## Modified 4/21/2022 by Trevor Holland
 ##
 
@@ -11,17 +12,34 @@ import random
 
 class NeuralNetworkPyspark():
 
-    def __init__(self, n_inputs, n_hiddens_per_layer, n_outputs, activation_function='tanh'):
+    '''Initialization functions'''
+    def __init__(self, n_inputs, n_outputs, n_hiddens_per_layer=[3, 10, 10], activation_function='tanh'):
         self.input_layer = n_inputs # number of neurons in the input layer
         self.hidden_layer = n_hiddens_per_layer # number of neurons in the hidden layers (Custom)
         self.output_layer = n_outputs # number of neurons in the output layer
         self.act_func = activation_function #activation function
 
-        # Initialize Weights
-        self.W1 = np.random.rand(input_layer, hidden_layer) - 0.5 # Shape (784, 64)
-        self.W2 = np.random.rand(hidden_layer, output_layer) - 0.5 # Shape (64, 2)
-        self.B1 = np.random.rand(1, hidden_layer) - 0.5 # Shape (1, 64)
-        self.B2 = np.random.rand(1, output_layer) - 0.5 # Shape (1, 2)
+        # Set self.n_hiddens_per_layer to [] if argument is 0, [], or [0]
+        if n_hiddens_per_layer == 0 or n_hiddens_per_layer == [] or n_hiddens_per_layer == [0]:
+            self.n_hiddens_per_layer = []
+        else:
+            self.n_hiddens_per_layer = n_hiddens_per_layer
+
+        # Initialize weights, by first building list of all weight matrix shapes.
+        n_in = n_inputs
+        shapes = []
+        for nh in self.n_hiddens_per_layer:
+            shapes.append((n_in + 1, nh))
+            n_in = nh
+        shapes.append((n_in + 1, n_outputs))
+
+        # self.all_weights:  vector of all weights
+        # self.Ws: list of weight matrices by layer
+        self.all_weights, self.Ws = self.make_weights_and_views(shapes)
+
+        # Define arrays to hold gradient values.
+        # One array for each W array with same shape.
+        self.all_gradients, self.dE_dWs = self.make_weights_and_views(shapes)
 
         self.trained = False
         self.error_trace = []
@@ -30,6 +48,21 @@ class NeuralNetworkPyspark():
         self.Tmeans = None
         self.Tstds = None
 
+    def make_weights_and_views(self, shapes):
+        # vector of all weights built by horizontally stacking flatenned matrices
+        # for each layer initialized with uniformly-distributed values.
+        all_weights = np.hstack([np.random.uniform(size=shape).flat / np.sqrt(shape[0])
+                                 for shape in shapes])
+        # Build list of views by reshaping corresponding elements from vector of all weights
+        # into correct shape for each layer.
+        views = []
+        start = 0
+        for shape in shapes:
+            size =shape[0] * shape[1]
+            views.append(all_weights[start:start + size].reshape(shape))
+            start += size
+        return all_weights, views
+    
     '''Activation functions'''
     # General function to apply any activation function
     def activation(self, x, f):
@@ -40,8 +73,8 @@ class NeuralNetworkPyspark():
         return 1 / (1 + np.exp(-X))
 
     # Sigmoid prime function (used for backward prop)
-    def sigmoid_prime(self, x):
-        sig = self.sigmoid(x)
+    def sigmoid_prime(self, X):
+        sig = self.sigmoid(X)
         return sig * (1 - sig)
 
     def tanh(self, X):
@@ -60,8 +93,8 @@ class NeuralNetworkPyspark():
     
     '''Forward Pass functions'''
     # Compute the layer propagation before activation
-    def preforward(self, x, w, b):
-        return np.dot(x, w) + b
+    def preforward(self, X, w, b):
+        return np.dot(X, w) + b
 
     # Compute the layer propagation after activation
     # This is also equivalent to a predict function once model is trained
@@ -69,21 +102,17 @@ class NeuralNetworkPyspark():
         return self.activation(self.preforward(self.activation(self.preforward(x , W1, B1)), W2, B2))
 
     '''Backward propogation function'''
+    # Compute the derivative of the error regarding B1
+    def derivativeBias1(self, h_h, dB, W, f_prime):
+        return np.dot(dB, W.T) * f_prime(h_h)
+
     # Compute the derivative of the error regarding B2
-    def derivativeB2(self, y_pred, y_true, y_h, f_prime):
+    def derivativeBias2(self, y_pred, y_true, y_h, f_prime):
         return (y_pred - y_true) * f_prime(y_h)
 
-    # Compute the derivative of the error regarding W2
-    def derivativeW2(self, h, dB2):
-        return np.dot(h.T, dB2)
-
-    # Compute the derivative of the error regarding B1
-    def derivativeB1(self, h_h, dB2, W2, f_prime):
-        return np.dot(dB2, W2.T) * f_prime(h_h)
-
-    # Compute the derivative of the error regarding W1
-    def derivativeW1(self, x, dB1):
-        return np.dot(x.T, dB1)
+    # Compute the derivative of the error regarding Weights
+    def derivativeWeights(self, h, dB):
+        return np.dot(h.T, dB)
 
     '''Evalutaion functions'''
     def get_metrics(self, pred, true):
@@ -94,30 +123,22 @@ class NeuralNetworkPyspark():
     def sse(self, y_pred, y_true):
         return 0.5 * np.sum(np.power(y_pred - y_true, 2))
 
-    '''Training function'''
-    def train(self, X, T, num_iteration=50, learning_rate=0.1, verbose=True):
-        # Hyperparameters
-        # num_iteration = 50
-        # learning_rate = 0.1
+    # Mean squared error
+    def mse(self, Y, T):
+        mean_sq_error = np.mean((T - Y) ** 2)
+        return mean_sq_error
+
+    '''Training functions'''
+    def train(self, train_rdd, num_epochs=50, learning_rate=0.1, verbose=True):
         if self.act_func=='tanh':
             act = self.tanh
             der = self.tanh_prime
         elif self.act_func == 'sig':
             act = self.sigmoid
-            act = self.sigmoid_prime
+            der = self.sigmoid_prime
         else:
             act = self.relu
-            act = self.relu_prime
-
-        input_layer = 784 # number of neurones in the input layer (equal to image size)
-        hidden_layer = 64 # number of neurones in the hidden layer (Custom)
-        output_layer = 2 # number of neurones in the output layer (equal to the number of possible labels)
-
-        # Paramater Initialization
-        W1 = np.random.rand(input_layer, hidden_layer) - 0.5 # Shape (784, 64)
-        W2 = np.random.rand(hidden_layer, output_layer) - 0.5 # Shape (64, 2)
-        B1 = np.random.rand(1, hidden_layer) - 0.5 # Shape (1, 64)
-        B2 = np.random.rand(1, output_layer) - 0.5 # Shape (1, 2)
+            der = self.relu_prime
 
         # History over epochs
         cost_history = []
@@ -126,17 +147,19 @@ class NeuralNetworkPyspark():
         # Epoch Loop (mini batch implementation)
         print("Start Training Loop:")
 
-        for i in range(num_iteration):
+        for epoch in range(num_epochs):
             
             # Compute gradients, cost and accuracy over mini batch 
             
             ################## Notations ######################
             # x -> Input Image flatten of shape (1, 784)
-            # y* -> One hot label of shape (1, 2)
-            # h^ -> Forward prop from Input layer to hidden layer before activation (1, 64) using W1, B1 parm
-            # h -> Forward prop from Input layer to hidden layer after tanh activation (1, 64)
-            # y^ -> Forward prop from hidden layer to output layer before activation (1, 2) using W2, B2 parm
-            # y -> Forward prop from hidden layer to output layer after sigmoid activation (1, 2)
+
+            # y* -> One hot label of shape # need to eliminate this
+            
+            # h^ -> Forward prop from Input layer to hidden layer before activation using W1, B1 parm
+            # h -> Forward prop from Input layer to hidden layer after tanh activation 
+            # y^ -> Forward prop from hidden layer to output layer before activation using W2, B2 parm
+            # y -> Forward prop from hidden layer to output layer after sigmoid activation 
             # E -> Error between y and y* using SSE
             # Acc -> 1 is right prediction 0 otherwise
             # DE/D? -> Partial derivative of the Error regarding parmaters (B2, W2, B1, W1)
@@ -157,11 +180,11 @@ class NeuralNetworkPyspark():
 
             gradientCostAcc = train_rdd\
                                 .sample(False,0.7)\
-                                .map(lambda x: (x[0], self.preforward(x[0], W1, B1), x[1]))\
+                                .map(lambda x: (x[0], self.preforward(x[0], self.Ws[0][1:, :], self.Ws[0][0:1, :] ), x[1]))\
                                 .map(lambda x: (x[0], x[1], self.activation(x[1], act), x[2]))\
-                                .map(lambda x: (x[0], x[1], x[2], self.preforward(x[2], W2, B2), x[3]))\
+                                .map(lambda x: (x[0], x[1], x[2], self.preforward(x[2], self.Ws[1][1:, :], self.Ws[1][0:1, :]), x[3]))\
                                 .map(lambda x: (x[0], x[1], x[2], x[3], self.activation(x[3], act), x[4]))\
-                                .map(lambda x: (x[0], x[1], x[2], self.sse(x[4], x[5]), self.derivativeB2(x[4], x[5], x[3], der), int(np.argmax(x[4]) == np.argmax(x[5]))))\
+                                .map(lambda x: (x[0], x[1], x[2], self.mse(x[4], x[5]), self.derivativeB2(x[4], x[5], x[3], der), int(np.argmax(x[4]) == np.argmax(x[5]))))\
                                 .map(lambda x: (x[0], x[1], x[3], x[4], self.derivativeW2(x[2], x[4]) ,x[5]))\
                                 .map(lambda x: (x[0], x[2], x[3], x[4], self.derivativeB1(x[1],  x[3], W2, der) ,x[5]))\
                                 .map(lambda x: (x[1], x[2], x[3], x[4], self.derivativeW1(x[0], x[4]) ,x[5], 1)) \
@@ -190,15 +213,15 @@ class NeuralNetworkPyspark():
 
             # Display performances
             if verbose:
-                print(f"   Epoch {i+1}/{num_iteration} | Cost: {cost_history[i]} | Acc: {acc_history[i]*100} | Batchsize:{n}")
+                print(f"   Epoch {epoch+1}/{num_epochs} | Cost: {cost_history[epoch]} | Acc: {acc_history[epoch]*100} | Batchsize:{n}")
 
         print("Training end..")
         return self
 
     '''Use the model generated by train()'''
-    def use(self, X):
+    def use(self, use_rdd):
         # Use the trained model over the Testset and get Confusion matrix per class
-        metrics = test_rdd.map(lambda x: self.get_metrics(np.round(self.predict(x[0], W1, B1, W2, B2)), x[1]))\
+        metrics = use_rdd.map(lambda x: self.get_metrics(np.round(self.predict(x[0], W1, B1, W2, B2)), x[1]))\
                         .reduce(lambda x, y: x + y)
 
         # For each class give TP, FP, FN, TN and precision, and recall, and F1 score
