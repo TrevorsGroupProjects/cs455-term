477ec2f5195154ca00406b9437263c884b0def8b
rmartinez2 <rmartinez@knights.ucf.edu>
2022-04-21 20:11:14 -0600

    Code to create a new column from county and abbrev state code

--- src/main/dropout_data_spark_job.py
+++ src/main/dropout_data_spark_job.py
@@ -25,22 +25,64 @@ if __name__ == "__main__":
     input_path = sys.argv[1]
     
     if ".csv" not in input_path:
-        print("Currently only targets .csv files")
+        print("\n\n!!!!!!!Currently only targets .csv files....EXITING!!!!!!\n\n")
         spark.stop()
+        sys.exit(-1)
         
-    df = spark.read().csv(input_path)
+    df = spark.read.option(header=True).csv(input_path)
+    
+    state_by_state_postal_codes = {"Alabama": "AL", "Alaska": "AK", "Arizona" :	"AZ", 
+                                   "Arkansas" : "AR",
+                                   "California": "CA",
+                                   "Colorado": "CO",
+                                   "Connecticut": "CT",
+                                   "Delaware":	"DE",
+                                   "Ohio": "OH",
+                                   "Florida": 	"FL",
+                                   "Georgia": "GA",
+                                   "Hawaii": "HI",
+                                   "Idaho":	"ID",
+                                   "Illinois" : "IL",
+                                   "Indiana": "IN",
+                                   "Iowa":	"IA",
+                                   "Kansas": "KS",
+                                   "Kentucky":	"KY",
+                                   "Louisiana":"LA",
+                                   "Maine": "ME",
+                                   "Maryland":	"MD",
+                                   "Massachusetts": "MA",
+                                   "Michigan": "MI",
+                                   "Minnesota": "MN",
+                                   "Mississippi": "MS",
+                                   "Missouri": "MO",
+                                   "Montana": "MT",
+                                   "Nebraska": "NE",
+                                   "Nevada": "NV",
+                                   "New Hampshire": "NH",
+                                   "New Jersey": "NJ",
+                                   "New Mexico": "NM",
+                                   "New York":	"NY",
+                                   "North Carolina": "NC",
+                                   "North Dakota": "ND",
+                                   "Oklahoma": "OK",
+                                   "Oregon": "OR",
+                                   "Pennsylvania": "PA",
+                                   "Rhode Island": "RI",
+                                   "South Carolina": "SC",
+                                   "South Dakota": "SD",
+                                   "Tennessee": "TN",
+                                   "Texas": "TX",
+                                   "Utah": "UT",
+                                   "Vermont": "VT",
+                                   "Virginia": "VA",
+                                   "Washington": "WA",
+                                   "West Virginia": "WV",
+                                   "Wisconsin": "WI",
+                                   "Wyoming": "WY"}
+    
+    #Add a new column for the county and state postal abbrev
+    df["County-State"] = df["County"] + state_by_state_postal_codes[df["State"]]
     df.show()
-    # output_path = sys.argv[2]
-    # Reads into a dataframe.
-    # Need to figure out the format.
-    #df = spark.read.format("libsvm").load(input_path).cache()
-
-    # dataFrame = spark.createDataFrame([
-    #     (0, Vectors.dense([1.0, 0.5, -1.0]),),
-    #     (1, Vectors.dense([2.0, 1.0, 1.0]),),
-    #     (2, Vectors.dense([4.0, 10.0, 2.0]),)
-    # ], ["id", "features"])
-
-    # newFrame.coalesce(1).write.csv(output_path) # ouptut to file
+    
 
     spark.stop()
