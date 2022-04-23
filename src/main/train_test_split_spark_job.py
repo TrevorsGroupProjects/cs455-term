c21625534023758727aa5bc31bd062ca6c7d2138
rmartinez2 <rmartinez@knights.ucf.edu>
2022-04-23 13:44:02 -0600

    Adding new sparkjob to train_test split the data

---
+++ src/main/train_test_split_spark_job.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 09:32:46 2022
+
+@author: Rene
+"""
+
+import os
+import sys
+import tempfile
+import shutil
+
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import Normalizer
+from pyspark.ml.linalg import Vectors
+from pyspark.mllib.stat import Statistics
+from pyspark.mllib.util import MLUtils
+
+import NeuralNetworkPyspark
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: predict_from_svi.py <file>", file=sys.stderr)
+        sys.exit(-1)
+
+    spark = SparkSession\
+        .builder\
+        .appName("PredictEducation")\
+        .getOrCreate()
+
+    input_path = sys.argv[1]
+    # output_path = sys.argv[2]
+    # Reads into a dataframe.
+    # Need to figure out the format.
+    #df = spark.read.format("libsvm").load(input_path).cache()
+    
+    df = spark.read.option("header", True).csv(input_path)
+    
+    df = df.drop("County-State")
+    
+    columns = df.columns
+    
+    print(columns)
+    
+    if "SVI" not in columns:
+         print("\n\n!!!!Missing SVI In Columns, exiting\n\n")
+        sys.exit(-1)
+    
+        
+    columns.remove("SVI")
+    columns.insert(0, "SVI")
+    df_reordered = df.select(columns)
+    df_reordered.show()
+    
+    
+        
+    
+
+    spark.stop()
+
+
+
