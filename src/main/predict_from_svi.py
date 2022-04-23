7e0d34199d1882ad11ba84270bd6ce2c280c5a17
rmartinez2 <rmartinez@knights.ucf.edu>
2022-04-23 12:51:34 -0600

    Updated predict from svi function

--- src/main/predict_from_svi.py
+++ src/main/predict_from_svi.py
@@ -10,6 +10,8 @@ from pyspark.ml.linalg import Vectors
 from pyspark.mllib.stat import Statistics
 from pyspark.mllib.util import MLUtils
 
+import NeuralNetworkPyspark
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: predict_from_svi.py <file>", file=sys.stderr)
@@ -24,14 +26,16 @@ if __name__ == "__main__":
     # output_path = sys.argv[2]
     # Reads into a dataframe.
     # Need to figure out the format.
-    df = spark.read.format("libsvm").load(input_path).cache()
-
-    # dataFrame = spark.createDataFrame([
-    #     (0, Vectors.dense([1.0, 0.5, -1.0]),),
-    #     (1, Vectors.dense([2.0, 1.0, 1.0]),),
-    #     (2, Vectors.dense([4.0, 10.0, 2.0]),)
-    # ], ["id", "features"])
-
-    # newFrame.coalesce(1).write.csv(output_path) # ouptut to file
+    #df = spark.read.format("libsvm").load(input_path).cache()
+    
+    df = spark.read.csv(input_file)
+    
+    df = df.drop("County-State")
+    
+    columns = df.columns
+    
+    print(columns)
+    
+    
 
     spark.stop()
